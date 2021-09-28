92028cca8ae5e529ac0330edb331668c32b02252
ADamian17 <adonisdesuvikdo@gmail.com>
2021-09-28 12:04:00 -0700

    added views product/index show and 404 page

--- server.js
+++ server.js
@@ -11,23 +11,50 @@ const app = express();
 const products = require('./models/product_model');
 
 
-// Routes
+/* == App configs == */
+app.set('view engine', 'ejs');
+
+/* == middlewares == */
+app.use(express.static('public'))
+
+/* == Routes == */
 app.get('/', function(req, res) {
     res.send(`<h1>Welcome To Sell it Up</h3>`)
 })
 
+/* == index route == */
 app.get('/products', (req, res) => {
     products.find((products) => {
-        res.send(products);
+
+        const context = {
+            products,
+        }
+
+        res.render('product/index', context);
     });
 });
 
-app.get('/products/:id', (req, res) => { 
+/* == Show == */
+app.get('/products/:id', (req, res, next) => { 
     products.findById(req.params.id, ( error, foundProduct ) => {
-        if (error) return console.log(error);
+        if (error) {
+            console.log(error)
+            req.error = error
+            return next()
+        };
 
-        res.send(foundProduct);
+        const context = {
+            product: foundProduct,
+        }
+
+        res.render('product/show', context);
     });
+});
+
+// 404
+app.get("/*", (req, res) => {
+    const context = { error: req.error };
+    return res.status(404).render("404", context);
 });
 
 // My server listening for cool stuff to happen
